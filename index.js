ec98053337dc59892b552c3d144de52c137e8317
Art-sys-86 <hithukten@gmail.com>
2025-11-22 21:53:49 +0600

    js

--- index.js
+++ index.js
@@ -1,18 +1,62 @@
-const inputUser = document.getElementsByClassName(`input`);
+const inputUser = document.querySelector(`input`);
 const numOne = document.getElementById(`one`);
 const numTwo = document.getElementById(`two`);
+const add = document.getElementById(`plus`);
+const sub = document.getElementById(`minus`);
+const equal = document.getElementById(`equal`);
 const delC = document.getElementById(`del`);
-
-console.log(inputUser);
 
 let firstNumber = '';
 let secondNumber = '';
 let operator = '';
+let waitingForNum = false;
 
 const maxLength = 12;
 
 numOne.addEventListener('click', () => {
+    if(inputUser.value.length >= maxLength){
+        return;
+    }
+    if(waitingForNum){
     inputUser.value = 1;
+    waitingForNum = false;
+    } else{
+        inputUser.value += 1;
+    }
 })
+
+add.addEventListener('click', () => {
+    firstNumber = parseFloat(inputUser.value);
+    inputUser.value = "+";
+    operator = "+";
+    waitingForNum = true;
+})
+
+sub.addEventListener('click', ()=> {
+    firstNumber = parseFloat(inputUser.value);
+    inputUser.value = "-";
+    operator = "-";
+    waitingForNum = true;
+})
+
+equal.addEventListener('click', () =>{
+    secondNumber = parseFloat(inputUser.value);
+    if(operator === "+"){
+        inputUser.value = firstNumber + secondNumber;
+        firstNumber = '';
+        operator = '';
+    }
+    if(operator === "-"){
+        inputUser.value = firstNumber - secondNumber;
+        firstNumber = '';
+        operator = '';
+    }
+})
+delC.addEventListener('click', () => {
+    inputUser.value = '';
+})
+
+
+
 
 
