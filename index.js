ea2239f3a7a6391c340c19a27e9ff47822207c43
Art-sys-86 <hithukten@gmail.com>
2025-11-26 17:49:45 +0600

    fixed chaining method after 3 days

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const numOne = document.getElementById(`one`);
 const numTwo = document.getElementById(`two`);
 const add = document.getElementById(`plus`);
 const sub = document.getElementById(`minus`);
+const divide = document.getElementById('divide');
+const multiple = document.getElementById('times');
 const equal = document.getElementById(`equal`);
 const delC = document.getElementById(`del`);
 
-let firstNumber = '';
-let secondNumber = '';
-let currentNumber = '';
+let firstNumber = 0;
+let secondNumber = 0;
+let currentNumber = 0;
 let operator = '';
+console.log(operator);
 let waitingForNum = false;
 
 const maxLength = 12;
@@ -26,39 +29,76 @@ numOne.addEventListener('click', () => {
     }
 })
 
-add.addEventListener('click', () => {
-    firstNumber = parseFloat(inputUser.value); //
-    secondNumber = parseFloat(inputUser.value);
-    inputUser.value = "+";
-    operator = "+";
-    waitingForNum = true;
-    if(operator === "+"){
-        currentNumber = firstNumber + secondNumber;
+numTwo.addEventListener('click', () => {
+    if(inputUser.value.length >= maxLength){
+        return;
     }
-
+    if(waitingForNum){
+    inputUser.value = 2;
+    waitingForNum = false;
+    } else{
+        inputUser.value += 2;
+    }
 })
 
+
+
+//Operations 
+
+function cal(firstNumber, operator, secondNumber){
+    if(operator === "+"){
+        firstNumber = firstNumber + secondNumber;
+        return firstNumber;
+    }
+    else if(operator === "-"){
+        firstNumber = firstNumber - secondNumber;
+        return firstNumber;
+    }
+}
+
+add.addEventListener('click', () => {
+    //No operation = first Number is stored and operation is added! then we wait for second number!
+    if(operator === ""){
+        firstNumber = parseFloat(inputUser.value);
+        operator = "+";
+        waitingForNum = true;
+    }
+    //operator exists! the first number and the second Number is added!
+    //I didnt use if because to continue chaining.
+    else if(operator){
+        secondNumber = parseFloat(inputUser.value);
+        firstNumber = cal(firstNumber, operator, secondNumber);
+        waitingForNum = true;
+        inputUser.value = firstNumber; //update the display!
+    }
+});      
+
+
 sub.addEventListener('click', ()=> {
-    firstNumber = parseFloat(inputUser.value);
-    inputUser.value = "-";
-    operator = "-";
-    waitingForNum = true;
+    if(operator === ""){
+        firstNumber = parseFloat(inputUser.value);
+        operator  = "-";
+        waitingForNum = true;
+    }
+    else if(operator){
+        secondNumber = parseFloat(inputUser.value);
+        firstNumber = cal(firstNumber, operator, secondNumber);
+        waitingForNum = true;
+        inputUser.value = firstNumber;
+    }
 })
 
 equal.addEventListener('click', () =>{
-    inputUser.value = currentNumber;
-    if(inputUser.value === NaN){
-        return `Error!`;
-    }
+    secondNumber = parseFloat(inputUser.value);
+    console.log(secondNumber);
+    inputUser.value = cal(firstNumber, operator, secondNumber);
+    operator = '';
+    waitingForNum = true;
 })
 delC.addEventListener('click', () => {
-    inputUser.value = '';
-    currentNumber = '';
-    firstNumber = '';
-    secondNumber = '';
+    inputUser.value = "";
+    currentNumber = 0;
+    firstNumber = 0;
+    secondNumber = 0;
 })
-
-
-
-
 
